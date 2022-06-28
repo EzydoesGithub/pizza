8c8493daa5fdab57085b398f71dc7b8ad163b736
EzydoesGithub <81513373+EzydoesGithub@users.noreply.github.com>
2022-06-28 00:47:11 -0400

    Update pizza1.lua

--- pizza1.lua
+++ pizza1.lua
@@ -1,43 +1,22 @@
--- Gui to Lua
--- Version: 3.2
+local function fireproximityprompt(Obj, Amount, Skip)
+    if Obj.ClassName == "ProximityPrompt" then 
+        Amount = Amount or 1
+        local PromptTime = Obj.HoldDuration
+        if Skip then 
+            Obj.HoldDuration = 0
+        end
+        for i = 1, Amount do 
+            Obj:InputHoldBegin()
+            if not Skip then 
+                wait(Obj.HoldDuration)
+            end
+            Obj:InputHoldEnd()
+        end
+        Obj.HoldDuration = PromptTime
+    else 
+        error("userdata<ProximityPrompt> expected")
+    end
+end
 
--- Instances:
 
-local Pizza1 = Instance.new("ScreenGui")
-local Frame = Instance.new("ImageLabel")
-local TextLabel = Instance.new("TextLabel")
-
---Properties:
-
-Pizza1.Name = "Pizza1"
-Pizza1.Parent = game.CoreGui
-
-Frame.Name = "Frame"
-Frame.Parent = Pizza1
-Frame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-Frame.BackgroundTransparency = 1.000
-Frame.Position = UDim2.new(0.352763057, 0, 0.0595947541, 0)
-Frame.Size = UDim2.new(0, 388, 0, 59)
-Frame.Image = "rbxassetid://3570695787"
-Frame.ImageColor3 = Color3.fromRGB(25, 25, 25)
-Frame.ScaleType = Enum.ScaleType.Slice
-Frame.SliceCenter = Rect.new(100, 100, 100, 100)
-Frame.SliceScale = 0.120
-
-TextLabel.Parent = Frame
-TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-TextLabel.BackgroundTransparency = 1.000
-TextLabel.Size = UDim2.new(0, 388, 0, 59)
-TextLabel.Font = Enum.Font.GothamBold
-TextLabel.Text = "Start working by pressing \"E\" before timer ends. (3.5s)"
-TextLabel.TextColor3 = Color3.fromRGB(255, 255, 0)
-TextLabel.TextSize = 13.000
-	wait(1.5)
-	TextLabel.Text = "Start working by pressing \"E\" before timer ends. (2.0s)"
-	wait(1)
-	TextLabel.Text = "Start working by pressing \"E\" before timer ends. (1.0s)"
-	wait(1)
-	TextLabel.Text = "Start working by pressing \"E\" before timer ends. (0.0s)"
-	wait(0.2)
-        Frame.Visible = false
-
+fireproximityprompt(Workspace.MapStuff.Jobs.PizzaJob.JobBoard.Start.ProximityPrompt, 1, true) --game:GetService("Workspace").MapStuff.Jobs.PizzaJob.JobBoard.Start.ProximityPrompt
