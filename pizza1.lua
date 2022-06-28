75513d88fcc467a4b5c2aebd2abf11ea40e090e5
EzydoesGithub <81513373+EzydoesGithub@users.noreply.github.com>
2022-06-27 22:30:46 -0400

    Update pizza1.lua

--- pizza1.lua
+++ pizza1.lua
@@ -3,16 +3,17 @@
 
 -- Instances:
 
-local ScreenGui = Instance.new("ScreenGui")
+local Pizza1 = Instance.new("ScreenGui")
 local Frame = Instance.new("ImageLabel")
 local TextLabel = Instance.new("TextLabel")
 
 --Properties:
 
-ScreenGui.Parent = game.CoreGui
+Pizza1.Name = "Pizza1"
+Pizza1.Parent = game.CoreGui
 
 Frame.Name = "Frame"
-Frame.Parent = ScreenGui
+Frame.Parent = Pizza1
 Frame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 Frame.BackgroundTransparency = 1.000
 Frame.Position = UDim2.new(0.352763057, 0, 0.0595947541, 0)
@@ -28,7 +29,7 @@ TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 TextLabel.BackgroundTransparency = 1.000
 TextLabel.Size = UDim2.new(0, 388, 0, 59)
 TextLabel.Font = Enum.Font.GothamBold
-TextLabel.Text = "Start working by pressing \"E\" before timer ends. (3.5s)"
+TextLabel.Text = "Start working by pressing \\\"E\\\" before timer ends. (3.5s)"
 TextLabel.TextColor3 = Color3.fromRGB(255, 255, 0)
 TextLabel.TextSize = 13.000
 	wait(1.5)
@@ -38,5 +39,5 @@ TextLabel.TextSize = 13.000
 	wait(1)
 	TextLabel.Text = "Start working by pressing \"E\" before timer ends. (0.0s)"
 	wait(0.2)
-        game.CoreGui.ScreenGui:Remove()
+        game.CoreGui.Pizza1:Remove()
 
